4782126df7eb9d05318da4de8b696ea73cd348f7
Mauricio Generoso <mauricio.generoso@hotmail.com>
2024-12-18 11:36:44 +0000

    Log message received and joins (commented code - only when necessary)

--- streams/src/main/java/com/generoso/kafka/streams/StreamProcessor.java
+++ streams/src/main/java/com/generoso/kafka/streams/StreamProcessor.java
@@ -3,11 +3,13 @@ package com.generoso.kafka.streams;
 import com.generoso.kafka.streams.config.RocksDbConfig;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.kafka.KafkaStreamsMetrics;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.state.StoreBuilder;
@@ -26,20 +28,22 @@ public class StreamProcessor {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
-    private int counter = 0;
-
     @Bean
     public KafkaStreams kafkaStreams(MeterRegistry meterRegistry) {
         StreamsBuilder builder = new StreamsBuilder();
-        KTable<String, String> table1 = builder.table("titles", Materialized.with(Serdes.String(), Serdes.String()));
-        KTable<String, String> table2 = builder.table("rights", Materialized.with(Serdes.String(), Serdes.String()));
+        KTable<String, String> table1 = builder.table("titles", Consumed.with(Serdes.String(), Serdes.String()));
+        KTable<String, String> table2 = builder.table("rights", Consumed.with(Serdes.String(), Serdes.String()));
+
+//        table1.toStream().peek((k, v) -> System.out.println("Received key : " + k + " value : " + v));
+//        table2.toStream().peek((k, v) -> System.out.println("Received key : " + k + " value : " + v));
 
         KTable<String, String> joinedTable = table1.join(table2,
             (value1, value2) -> value1 + "-" + value2,
             Materialized.with(Serdes.String(), Serdes.String())
         );
 
-        joinedTable.toStream().foreach((k, v) -> counter++);
+//        joinedTable.toStream().foreach((k, v) ->
+//            System.out.println("merged key : " + k + " value : " + v));
 
         KafkaStreams streams = new KafkaStreams(builder.build(), properties());
         new KafkaStreamsMetrics(streams).bindTo(meterRegistry);
@@ -51,16 +55,13 @@ public class StreamProcessor {
         Properties properties = new Properties();
         properties.put(StreamsConfig.APPLICATION_ID_CONFIG, "streams-example");
         properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-//        properties.put(StreamsConfig.TOPOLOGY_OPTIMIZATION_CONFIG, StreamsConfig.OPTIMIZE);
+        properties.put(StreamsConfig.TOPOLOGY_OPTIMIZATION_CONFIG, StreamsConfig.OPTIMIZE);
 //        properties.put(StreamsConfig.STATESTORE_CACHE_MAX_BYTES_CONFIG, 0);
         properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
         properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+//        properties.put(ConsumerConfig.FETCH_MAX_BYTES_CONFIG, 17_000);
 //        properties.put(ProducerConfig.MAX_REQUEST_SIZE_CONFIG, 10_000_000);
 //        properties.put(StreamsConfig.ROCKSDB_CONFIG_SETTER_CLASS_CONFIG, RocksDbConfig.class);
         return properties;
-    }
-
-    public static StoreBuilder<?> keyValueStore(String storeName, Serde<?> keySerde, Serde<?> valueSerde) {
-        return keyValueStoreBuilder(persistentKeyValueStore(storeName), keySerde, valueSerde);
     }
 }
